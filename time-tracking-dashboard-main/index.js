65b8ebe85f6a5f22afc7022ab0e9ecb429f2342f
kartikku29 <kartikku098@gmail.com>
2022-06-24 19:21:44 +0530

    First Commit

---
+++ time-tracking-dashboard-main/index.js
@@ -0,0 +1,49 @@
+function daily() {
+
+    document.getElementById('work_h1').innerText = "5hrs";
+    document.getElementById('work_p').innerText = "7hrs";
+    document.getElementById('excercise_h1').innerText = "1hrs";
+    document.getElementById('excercise_p').innerText = "1hrs";
+    document.getElementById('play_h1').innerText = "1hrs";
+    document.getElementById('play_p').innerText = "2hrs";
+    document.getElementById('social_h1').innerText = "1hrs";
+    document.getElementById('social_p').innerText = "3hrs";
+    document.getElementById('study_h1').innerText = "0hrs";
+    document.getElementById('study_p').innerText = "1hrs";
+    document.getElementById('self_h1').innerText = "0hrs";
+    document.getElementById('self_p').innerText = "1hrs";
+}
+
+function weekly() {
+
+    document.getElementById('work_h1').innerText = "32hrs";
+    document.getElementById('work_p').innerText = "36hrs";
+    document.getElementById('excercise_h1').innerText = "4hrs";
+    document.getElementById('excercise_p').innerText = "5hrs";
+    document.getElementById('play_h1').innerText = "10hrs";
+    document.getElementById('play_p').innerText = "8hrs";
+    document.getElementById('social_h1').innerText = "5hrs";
+    document.getElementById('social_p').innerText = "10hrs";
+    document.getElementById('study_h1').innerText = "4hrs";
+    document.getElementById('study_p').innerText = "7hrs";
+    document.getElementById('self_h1').innerText = "2hrs";
+    document.getElementById('self_p').innerText = "2hrs";
+
+}
+
+function monthly() {
+
+    document.getElementById('work_h1').innerText = "103hrs";
+    document.getElementById('work_p').innerText = "128hrs";
+    document.getElementById('excercise_h1').innerText = "11hrs";
+    document.getElementById('excercise_p').innerText = "18hrs";
+    document.getElementById('play_h1').innerText = "23hrs";
+    document.getElementById('play_p').innerText = "29hrs";
+    document.getElementById('social_h1').innerText = "21hrs";
+    document.getElementById('social_p').innerText = "23hrs";
+    document.getElementById('study_h1').innerText = "13hrs";
+    document.getElementById('study_p').innerText = "19hrs";
+    document.getElementById('self_h1').innerText = "7hrs";
+    document.getElementById('self_p').innerText = "11hrs";
+
+}
